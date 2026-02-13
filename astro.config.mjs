2d4ebe2e9093f22c87416a1226bd2866e6f30ca2
MarianaDuran2903 <dmarianacamila@yahoo.com>
2026-02-13 15:29:10 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://MarianaDuran2903.github.io",
   vite: {
     plugins: [tailwindcss()],
   },
